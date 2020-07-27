83f6876baba9b6c13ec717c070a0d45ae9af8d1f
Aleksandra Pawlak <synowa8@gmail.com>
2020-07-27 13:25:49 +0200

    Work on project. Stage 1/6: Encrypted!
    https://hyperskill.org/projects/46/stages/245/implement

---
+++ Encryption-Decryption/task/src/encryptdecrypt/Main.java
@@ -0,0 +1,8 @@
+package encryptdecrypt;
+
+public class Main {
+//    "we found a treasure!" A, B, C, D, E|, F, G, H, I, J|, K, L, M|| N, O, P, |Q, R, S, T, U, |V, W, X, Y, Z.
+    public static void main(String[] args) {
+        System.out.println("dv ulfmw z givzhfiv!");
+    }
+}

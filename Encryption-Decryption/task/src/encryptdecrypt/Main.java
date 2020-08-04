31ca3cd6b5265d7033162c8b820f8050459b0940
Aleksandra Pawlak <synowa8@gmail.com>
2020-08-04 16:42:15 +0200

    Sorting numbers
    Arrays as parameters
    https://hyperskill.org/learn/step/2245

--- Encryption-Decryption/task/src/encryptdecrypt/Main.java
+++ Encryption-Decryption/task/src/encryptdecrypt/Main.java
@@ -5,17 +5,30 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-
         Scanner scanner = new Scanner(System.in);
         String targetOperation = scanner.nextLine();
         String message = scanner.nextLine();
         int key = scanner.nextInt();
-        EncryptDecrypt encryptDecrypt = new EncryptDecrypt(key);
 
+        char[] charsFromMessage = message.toCharArray();
         if ("enc".equals(targetOperation)) {
-            System.out.println(encryptDecrypt.encrypt(message));
+            for (char c : charsFromMessage) {
+//                if (c + key <= 122) {
+                    c = (char) (c + key);
+//                } else {
+//                    c = (char) (c + key - 26);
+//                }
+                System.out.print(c);
+            }
         } else if ("dec".equals(targetOperation)) {
-            System.out.println(encryptDecrypt.decrypt(message));
+            for (char c : charsFromMessage) {
+                if (c - key <= 122) {
+                    c = (char) (c - key);
+                } else {
+                    c = (char) (c - key + 26);
+                }
+                System.out.print(c);
+            }
         }
     }
 }
